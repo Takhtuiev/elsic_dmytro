e956d9990666d41de16d6b6c1d4af2980db3513d
Dmitriy <takhtuiev@gmail.com>
2025-05-16 22:32:35 +0200

    test

--- src/components/Filter/ContentCheckBoxList.js
+++ src/components/Filter/ContentCheckBoxList.js
@@ -1,6 +1,7 @@
 import {Checkbox, FormControlLabel, FormGroup, Typography} from "@mui/material";
 import React from "react";
 import {ID_EL_START} from "../../CONSTANTS/Constants";
+import {Skeleton} from "@mui/lab";
 
 const ContentCheckBoxList = ({ field, obj, updateObj }) => {
 
@@ -9,6 +10,24 @@ const ContentCheckBoxList = ({ field, obj, updateObj }) => {
         obj[value] = event.target.checked;
         updateObj(obj,field);
     };
+
+    // Если obj нет или он пустой — показываем скелетоны
+    if (!obj || Object.keys(obj).length === 0) {
+        // Отобразим 4 скелетона чекбоксов
+        return (
+            <FormGroup>
+                {Array.from({ length: 4 }).map((_, idx) => (
+                    <Skeleton
+                        key={idx}
+                        variant="rectangular"
+                        height={"1.5rem"}
+                        width={"70%"}
+                        sx={{ m: 1, borderRadius: 1 }}
+                    />
+                ))}
+            </FormGroup>
+        );
+    }
 
     return (
         <FormGroup>
