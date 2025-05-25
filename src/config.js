fa681ba441c6ef8063895a648f0c571f70bf2dcc
Dmitriy <takhtuiev@gmail.com>
2025-05-25 16:58:55 +0200

    test

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-// src/config.js
 
 // Определяем API_URL в зависимости от окружения
-export const API_URL = process.env.REACT_APP_API_URL; // Значение по умолчанию для локального запуска
+export const API_URL = process.env.BACKEND_API_URL; // Значение по умолчанию для локального запуска
 
+// Endpoints
 export const REFRESH_JWT = '/auth/refresh';
 export const LOGIN = '/auth/login';
 export const LOGOUT = '/auth/logout';
