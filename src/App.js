9f59b084b8f156d6fab355ed6ad263ad41c6b55e
Dmitriy <takhtuiev@gmail.com>
2025-06-01 10:54:41 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import Home from "./containers/Home/Home";
-import {lazy, Suspense} from "react";
-import {Provider} from "react-redux";
-import NotFound from "./containers/NotFoundPage/NotFound";
-import {store} from "./Store/store";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { Provider } from "react-redux";
+import { store } from "./Store/store";
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import {JwtProvider} from "./Providers/JwtProvider";
-import {ColorModeContextProvider} from "./Providers/ColorModeProvider";
+import { JwtProvider } from "./Providers/JwtProvider";
+import { ColorModeContextProvider } from "./Providers/ColorModeProvider";
 
 import LoadingSpinner from "./components/MyComponent/LoadingSpinnerBoard/LoadingSpinner";
 import NavigationTabs from "./components/Navigation/NavigationTabs";
 import Footer from "./containers/Footer/Footer";
-import DrinksList from "./containers/DrinksPage/DrinksList";
-import VariantsDrinksList from "./containers/DrinksPage/VariantsDrinksList";
-import BrandDetails from "./containers/DrinksPage/BrandDetails";
-import {Box} from "@mui/system";
+import { Box } from "@mui/system";
+import ScrollToTop from "./components/ScrollToTop";
 
-
-const UserList = lazy(() => import('./containers/UsersPage/UserList'));
+// 🔁 lazy load all route components
+const Home = lazy(() => import('./containers/Home/Home'));
+const DrinksList = lazy(() => import('./containers/DrinksPage/DrinksList'));
+const VariantsDrinksList = lazy(() => import('./containers/DrinksPage/VariantsDrinksList'));
 const DrinksDetails = lazy(() => import('./containers/DrinksPage/DrinksDetails'));
-const MyAccountDetails = lazy(() => import("./containers/UsersPage/MyAccountDetails"));
+const BrandDetails = lazy(() => import('./containers/DrinksPage/BrandDetails'));
+const UserList = lazy(() => import('./containers/UsersPage/UserList'));
+const MyAccountDetails = lazy(() => import('./containers/UsersPage/MyAccountDetails'));
+const NotFound = lazy(() => import('./containers/NotFoundPage/NotFound'));
 
 export default function App() {
 
@@ -31,6 +32,7 @@ export default function App() {
             <Provider store={store}>
                 <JwtProvider>
                     <BrowserRouter>
+                        <ScrollToTop />
                         <Box
                             display="flex"
                             flexDirection="column"
