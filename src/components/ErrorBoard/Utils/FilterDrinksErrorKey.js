f63c9b953262ec2db4fefc8ae9dff9754f4ffacd
Dmitriy <takhtuiev@gmail.com>
2025-07-20 15:00:05 +0200

    test

--- src/components/ErrorBoard/Utils/FilterDrinksErrorKey.js
+++ src/components/ErrorBoard/Utils/FilterDrinksErrorKey.js
@@ -4,6 +4,12 @@ export function filterItemErrorKey(item, errorData) {
     if (!item) { // Если item === null, то возвращаем как есть
         return errorData
     }
+
+    // Если errorData не объект (строка, число и т.д.) или это массив — возвращаем как есть
+    if (typeof errorData !== 'object' || errorData === null || Array.isArray(errorData)) {
+        return errorData;
+    }
+
     // Получаем все ключи из объекта `item` и его `variants`
     const allKeys = Object.keys(item).flatMap(key =>
         key === 'variants'
