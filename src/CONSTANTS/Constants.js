99975d39ec2894d1e4c5437970bd00875df12fd5
Dmitriy <takhtuiev@gmail.com>
2026-09-06 23:57:14 +0200

    test

--- src/CONSTANTS/Constants.js
+++ src/CONSTANTS/Constants.js
@@ -1,21 +1,6 @@
 
 export const ID_EL_START = 'my-element-'
 
-export const UPLOAD_IMAGE = {maxWidth: 1920, maxHeight: 1080}
-
-export const WATERMARK = null;
-export const FONT_WATERMARK = 'Roboto';
-
-
-export const USER_COLUMNS = {
-    id: 'ID',
-    username: 'Name',
-    password: 'Password',
-    email: 'Email',
-    phone: 'Phone',
-    authorities: 'Authorities',
-    enabled: 'Enable'
-};
 
 export const TOP_MENU = [
     {name:'Biegeberechnung', href:'/biegeberechnung'},
